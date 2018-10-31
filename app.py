c5a9ad9e38375996ea57bf40ecf5301e68144e71
jjmachan <jamesjithin97@gmail.com>
2018-10-31 23:36:45 +0530

    added the basic templates and fixed basic login

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for, session
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -10,19 +12,35 @@ def hello_world(name=None):
 
 @app.route('/')
 def index():
-    return 'Index'
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else: 
+        return redirect(url_for('home'))
+
 @app.route('/search')
 def search():
     return 'you can search for people here'
-@app.route('/home/<username>')
-def userviews(username):
-    return 'hello %s' % username
 
-@app.route('/<username>/<int:post_id>')
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method=='POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'password':
+            error = 'Invalid Credentials'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/u/<username>/<int:post_id>')
 def posts(username, post_id):
     return 'This is post %d' % post_id
 
-@app.route('/<username>')
+@app.route('/u/<username>')
 def profile(username):
     return 'This is %s ' %username
 
